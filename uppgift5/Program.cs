909fbd68aa44de693e095408c1a073fef52cbd50
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-07 14:20:50 +0200

    jag började med att göra loop och if satts

---
+++ uppgift5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             bool x = true;
+            while (x == true)  //crash 
+                
+            {
+                Console.WriteLine("skriv Avsluta för att avsluta");
+                string close = Console.ReadLine();
+
+                if ( close == "Avlsuta" && close == "avsluta" && close == "AVSLUTA" )
+                {
+                    x = false;
+                }
+                
+                else
+                {
+                    x = true;
+                }
+
+            }
+        }
+    }
+}
