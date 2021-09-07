9ca86c638a562578c43df716122a57da31650f7d
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-07 18:42:33 +0200

    har gjort klart uppgiften

---
+++ uppgift6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace uppgift6
+{
+    class program 
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("vilket tal mellan 1 och 21 tänker jag på? ");
+            Random rnd = new Random();
+            int number = rnd.Next(1, 21);
+            bool x = true;
+            while (x == true)
+            {
+                int anwser = Convert.ToInt32(Console.ReadLine()); 
+
+                if (anwser == number )
+                {
+                    Console.WriteLine($"bra gissat, du svarade rätt, talet är {number}");
+                    x = false;
+                }
+                else
+                {
+                    Console.WriteLine("försök igen! "); 
+                }
+
+            }
+        }
+
+    }
+}
