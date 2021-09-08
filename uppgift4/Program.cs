8a0dcca40601347fcb00d9b95cc0344442c83103
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-08 14:28:12 +0200

    initialize startup

---
+++ uppgift4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift4
+{
+    class Program
+    {                                                       //1. Låt användaren mata in en mening
+                                                            //2. Skriv ut varje unik bokstav på en egen rad, och ange hur ofta den förekommer
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sentence = Console.ReadLine();
+            
+
+        }
+    }
+}
