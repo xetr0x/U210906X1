100d259da5dfb92bc7c91a01b837414327d68b50
Kevin Wilusz <kwilusz10@gmail.com>
2021-09-08 14:27:32 +0200

    initialize startup

--- uppgift2/Program.cs
+++ uppgift2/Program.cs
@@ -3,10 +3,25 @@
 namespace uppgift2
 {
     class Program
-    {
-        static void Main(string[] args)
+    {                                                                                                   //Låt användaren mata in en mening.
+                                                                                                        // Skriv ut varje ord på en egen rad, i storleksordning (längd på ordet)
+        static void Main(string[] args)                                                                 //Skriv ut varje ord på en egen rad, i omvänd ordning (baklänges) 
         {
             Console.WriteLine("Hello World!");
+            string sentence = Console.ReadLine();
+
+            char[] separators = new char[] { ' ', '.' };
+
+            string[] subs = string.Split(separators, StringSplitOptions.RemoveEmptyEntries); 
+
+            foreach (var sub in subs)
+            {
+                Console.Write($"substring: {sub}");
+            }
+        }
+           
+
+
         }
     }
 }
